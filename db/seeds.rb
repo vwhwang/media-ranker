cf67ce53adb7e41f7ba205a5848bfa9875dbb3e0
vwhwang <vwhwang38@gmail.com>
2020-05-14 15:15:53 -0700

    added work model and seed the data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,30 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'csv'
+
+WORK_FILE = Rails.root.join('db','works-seeds.csv',)
+
+work_failures = []
+CSV.foreach(WORK_FILE, :headers => true ) do |row|
+  work = Work.new
+  work.id = row['id']
+  work.category = row['category']
+  work.title = row['title']
+  work.creator = row['creator']
+  work.publication_year = row['publication_year']
+  work.description = row['description']
+  successful = work.save
+
+  if !successful
+    work_failures << work 
+    puts "Failed to save work: #{work.inspect}"
+  else   
+    puts "Created work: #{work.inspect}"
+  end 
+  
+end 
+
+puts "Added #{Work.count} work records"
+puts "#{work_failures.length} works failed to save"
