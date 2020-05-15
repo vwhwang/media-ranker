6ec90e9de89b373134a7516170e5480a6b77508b
vwhwang <vwhwang38@gmail.com>
2020-05-14 21:05:48 -0700

    added edit/update/new/destory for work and home page

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -4,5 +4,73 @@ class WorksController < ApplicationController
     @works = Work.all
   end 
   
+  def show 
+    work_id = params[:id]
+    @work = Work.find_by(id: work_id)
+
+    if @work.nil? 
+      redirect_to works_path
+      return 
+    end 
+  end 
+
+  def new 
+    @work = Work.new
+  end 
+  
+  def create
+    @work = Work.new(work_params)
+
+    if @work.save
+      redirect_to work_path(@work.id)
+      return 
+    else  
+      render :new 
+      return 
+    end 
+  end 
+
+  def edit 
+    @work = Work.find_by(id: params[:id])
+
+    if @work.nil?
+      head :not_found
+      return 
+    end 
+  end 
+
+  def update
+    @work = Work.find_by(id: params[:id])
+
+    if @work.nil?
+      head :not_found
+      return 
+    elsif @work.update(work_params)
+      redirect_to work_path(@work.id)
+      return 
+    else  
+      render :edit 
+      return 
+    end 
+  end 
+
+  def destory 
+    @work = Work.find_by(id: params[:id])
+
+    if @work.nil?
+      head :not_found
+      return
+    else   
+      @work.destory
+      redirect_to works_path
+      return 
+    end 
+  end 
+
+  private 
+
+  def work_params
+    return params.require(:work).permit(:category, :title, :publication_year, :creator, :description)
+  end 
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
+  get 'home/index', to: 'home#index', as: 'home'
+  root to: 'home#index'
   resources :works 
 end
