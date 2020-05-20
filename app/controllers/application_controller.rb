06a91ed054eed5eeb156c2138c0a94e250a5d4ae
vwhwang <vwhwang38@gmail.com>
2020-05-20 14:00:15 -0700

    added controller filter for login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+
+
+  def require_login
+    if session[:user_id].nil?
+      flash[:error] = "You must login to do that"
+      redirect_back(fallback_location: root_path)
+    end
+  end
+
+
 end
--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,14 +1,12 @@
 class VotesController < ApplicationController
 
+  before_action :require_login
+
+
   def upvote
 
-    if session[:user_id].nil? 
-      flash[:error] = "You must login to do that"
-      redirect_to work_path(id: params[:work_id])
-      return 
-    end 
-
     @vote = Vote.all.find_by(work_id:params[:work_id] ,user_id: session[:user_id])
+    
     if @vote.nil? 
       Vote.create(work_id:params[:work_id] ,user_id: session[:user_id]) 
       flash[:message] = "Successfuly upvoted!"
